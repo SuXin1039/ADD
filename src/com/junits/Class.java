5612639e4829e6aac375d3ead2aada7700b375e6
SuXin1039 <sx1224@vip.qq.com>
2015-09-07 02:05:31 +0800

    项目提交

---
+++ src/com/junits/Class.java
@@ -0,0 +1,15 @@
+package com.junits;
+
+public class Class {
+
+	public Class() {
+	}
+	
+	public int add(int a,int b){
+		int sum;
+		sum = a+b;
+		return sum;
+	}
+	
+}
+                                          
